7c4fb92fe9d442aef70935f482352458ea16e37f
unknown <kinchoily@hotmail.com>
2022-06-15 20:02:06 +0100

    Update notes on 'Working with arrays'

--- 10. Working With Arrays/script.js
+++ 10. Working With Arrays/script.js
@@ -311,4 +311,101 @@ currenciesUnique.forEach((value, _, map) => {
 
 // ===================================================================================================================================
 
+// ======================================================== Coding challenge #1 ======================================================
+/*
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners 
+about their dog's age, and stored the data into an array (one array for each). For 
+now, they are just interested in knowing whether a dog is an adult or a puppy.
+A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years 
+old.
+Your tasks:
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages 
+('dogsJulia' and 'dogsKate'), and does the following things:
+1. Julia found out that the owners of the first and the last two dogs actually have 
+cats, not dogs! So create a shallow copy of Julia's array, and remove the cat 
+ages from that copied array (because it's a bad practice to mutate function 
+parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 
+is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 
+�
+")
+4. Run the function for both test datasets
+*/
 
+const checkDogs = (dogJulia, dogKate) => {
+  const juliaDogOnly = dogJulia.slice(1,3);
+  const dogs = [...juliaDogOnly, ...dogKate];
+
+  // concat method
+  // const dogData = juliaDogData.concat(dogArr2)
+
+  // console.log(dogData);
+  dogs.forEach((dog, i) => {
+    if (dog > 3) {
+      console.log(`Dog #${i + 1}: Is an adult`)
+    } else {
+      console.log(`Dog #${i + 1}: Is still a puppy `)
+    }
+  })
+}
+
+checkDogs( [3, 5, 2, 12, 7],  [4, 1, 15, 8, 3])
+
+// ===================================================================================================================================
+
+// =========================================================== Map() Method ==========================================================
+/*
+map() 
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+- Method creates a new array populated with the results of calling a provided function on every element in the calling array.
+- Method creates a copy of the original array, which allows us to make changes (with provided function) to the copied array, keeping original array safe.
+
+If we need extra lines of code (block) we need to use {curly braces} and explicit return
+map(() => {
+
+  const name = "Sarah"
+  return `hello ${name}`
+ 
+});
+
+If we just want to return we can use implicit return (with out return keyword)
+map(() =>
+
+  "something"
+  
+);
+
+*/
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+// Explicit return 
+const convertedEurToUsd = movements.map((cur) => {
+  // return the values we want the new array to have
+  return cur * eurToUsd
+});
+console.log(convertedEurToUsd);
+
+// One liner from above (eww)
+const convertedEurToUsdOneliner = movements.map(cur => cur * eurToUsd);
+console.log(convertedEurToUsdOneliner);
+
+// Example with forEach
+const movementArr = [];
+movements.forEach((movement) => {
+  movementArr.push(movement * eurToUsd);
+})
+console.log(movementArr);
+
+// Using index (implicit return)
+const movementDiscription = movements.map(
+  (movement, index) =>
+    `Movement ${index}: You have ${movement > 0 ? "deposited" : "withdrawn"}: ${Math.abs(
+      movement
+    )}`
+);
+
+console.log(movementDiscription);
+// ===================================================================================================================================
