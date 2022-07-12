b3118fe9fb14e646d90ef4272d9ddd3b0f1f514d
unknown <kinchoily@hotmail.com>
2022-07-12 16:30:44 +0100

    Update notes on working with arrays and add code Challenge #4

--- 10. Working With Arrays/script.js
+++ 10. Working With Arrays/script.js
@@ -1310,41 +1310,159 @@ console.log(allDeposits, allWithdrawals);
 
 // .4
 const convertTitleCase = (string) => {
-
   const exceptions = ["a", "an", "the", "and", "but", "or", "on", "in", "with"];
 
-  // The  mess
-  // const titleCase = string
-  //   .toLowerCase()
-  //   .split(" ")
-  //   .map((word) => {
-  //     if (!exceptions.includes(word)) {
-  //       return word[0].toUpperCase() + word.slice(1);
-  //     } else {
-  //       return word;
-  //     }
-  //   });
+  /*
+  The mess
+  const titleCase = string
+    .toLowerCase()
+    .split(" ")
+    .map((word) => {
+      if (!exceptions.includes(word)) {
+        return word[0].toUpperCase() + word.slice(1);
+      } else {
+        return word;
+      }
+    });
 
-  // return titleCase[0][0].toUpperCase() + titleCase[0].slice(1) + " " + titleCase.slice(1).join(" ");
+  return titleCase[0][0].toUpperCase() + titleCase[0].slice(1) + " " + titleCase.slice(1).join(" ");
+
+  */
 
   const capitalize = (str) => {
     return str[0].toUpperCase() + str.slice(1);
-  }
+  };
 
   const titleCase = string
     .toLowerCase()
     .split(" ")
-    .map((word) =>
-      exceptions.includes(word) ? word : capitalize(word)
-    ).join(" ");
+    .map((word) => (exceptions.includes(word) ? word : capitalize(word)))
+    .join(" ");
 
   return capitalize(titleCase);
- 
-}
+};
 
 console.log(convertTitleCase("this is a nice title"));
 // This Is a Nice Title
 console.log(convertTitleCase("this is a LONG title but not too long"));
 console.log(convertTitleCase("and here is another title with an EXAMPLE"));
+
+// ===================================================================================================================================
+
+// =========================================================== Challenge #4 ==========================================================
+/*
+Julia and Kate are still studying dogs, and this time they are studying if dogs are 
+eating too much or too little.
+Eating too much means the dog's current food portion is larger than the 
+recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% 
+above and 10% below the recommended portion (see hint).
+Your tasks:
+
+1. Loop over the 'dogs' array containing dog objects, and for each dog, calculate 
+the recommended food portion and add it to the object as a new property. Do 
+not create a new array, simply loop over the array. Forumla: 
+recommendedFood = weight ** 0.75 * 28. (The result is in grams of 
+food, and the weight needs to be in kg)
+
+2. Find Sarah's dog and log to the console whether it's eating too much or too 
+little. Hint: Some dogs have multiple owners, so you first need to find Sarah in 
+the owners array, and so this one is a bit tricky (on purpose) �
+
+3. Create an array containing all owners of dogs who eat too much 
+('ownersEatTooMuch') and an array with all owners of dogs who eat too little 
+('ownersEatTooLittle').
+
+4. Log a string to the console for each array created in 3., like this: "Matilda and 
+Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat 
+too little!"
+
+5. Log to the console whether there is any dog eating exactly the amount of food 
+that is recommended (just true or false)
+
+6. Log to the console whether there is any dog eating an okay amount of food 
+(just true or false)
+
+7. Create an array containing the dogs that are eating an okay amount of food (try 
+to reuse the condition used in 6.)
+
+8. Create a shallow copy of the 'dogs' array and sort it by recommended food 
+portion in an ascending order (keep in mind that the portions are inside the 
+array's objects �)
+
+The Complete JavaScript Course 26
+Hints:
+§ Use many different tools to solve these challenges, you can use the summary 
+lecture to choose between them �
+§ Being within a range 10% above and below the recommended portion means: 
+current > (recommended * 0.90) && current < (recommended * 
+1.10). Basically, the current portion should be between 90% and 110% of the 
+recommended portion
+*/
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
+  { weight: 8, curFood: 200, owners: ["Matilda"] },
+  { weight: 13, curFood: 275, owners: ["Sarah", "John"] },
+  { weight: 32, curFood: 340, owners: ["Michael"] },
+];
+
+// 1.
+dogs.forEach((dog) => {
+  dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28);
+});
+console.log(dogs);
+
+// 2.
+const sarahDog = dogs.find((dog) => dog.owners.includes("Sarah"));
+console.log(sarahDog);
+console.log(
+  `Sarah dog is eating too ${
+    sarahDog.curFood > sarahDog.recommendedFood ? "much" : "little"
+  }`
+);
+
+// 3.
+const ownersEatTooMuch = dogs
+  .filter((dog) => dog.curFood > dog.recommendedFood)
+  .flatMap((owner) => owner.owners);
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs
+  .filter((dog) => dog.curFood > dog.recommendedFood)
+  .flatMap((owner) => owner.owners);
+console.log(ownersEatTooLittle);
+
+// 4.
+console.log(`${ownersEatTooMuch.join(" and ")} dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(" and ")} dogs eat too little!`);
+
+// 5.
+console.log(dogs.some((dog) => dog.curFood === dog.recommendedFood));
+
+// 6.
+const isFoodAmountOk = (dog) => {
+  return (
+    dog.curFood > dog.recommendedFood * 0.9 &&
+    dog.curFood < dog.recommendedFood * 1.1
+  );
+};
+
+console.log(isFoodAmountOk(dogs));
+
+// 7.
+const dogsEatingOkAmount = dogs.filter((dog) => isFoodAmountOk(dog));
+console.log(dogsEatingOkAmount);
+
+// 8.
+// slice to make a shallow copy of an array as to not mutate the original when using sort method
+const dogsAccending = dogs.slice().sort((a, b) => {
+  if (a.curFood < b.recommendedFood) return -1;
+  if (a.curFood > b.recommendedFood) return 1;
+  return 0;
+});
+
+console.log(dogs);
+console.log(dogsAccending);
 
 // ===================================================================================================================================
