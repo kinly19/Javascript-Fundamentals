4691ac2bf420897fdbc0397875bb0f238d9c8951
unknown <kinchoily@hotmail.com>
2022-08-09 21:13:02 +0100

    Implement 'Slider component'

--- 12. Advanced-DOM/script.js
+++ 12. Advanced-DOM/script.js
@@ -229,6 +229,104 @@ imageTargets.forEach(img => {
   imageObserver.observe(img);
 });
 
+// Slider component
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const slides = document.querySelectorAll(".slide");
+const slider = document.querySelector(".slider");
+const dotsContainer = document.querySelector(".dots");
+
+let currentSlide = 0;
+const maxSlide = slides.length;
+
+// Functions
+// Creating and adding dots into DOM
+const createDots = () => {
+  slides.forEach((_, index) => {
+    dotsContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide=${index}></button>`
+    );
+  });
+};
+
+const activateDot = slide => {
+  document.querySelectorAll('.dots__dot').forEach(dot => {
+    dot.classList.remove('dots__dot--active');
+  });
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+
+/*
+const activateDot2 = slide => {
+  document.querySelectorAll('.dots__dot').forEach(dot => {
+    dot.classList.remove('dots__dot--active');
+  });
+
+  document.querySelectorAll('.dots__dot').forEach(dot => {
+    if (+dot.dataset.slide === +slide) dot.classList.add('dots__dot--active');
+  });
+};
+activateDot2(currentSlide)
+*/
+
+const goToSlide = slide => {
+  slides.forEach((s, index) => {
+    s.style.transform = `translateX(${100 * (index - slide)}%)`;
+  });
+};
+
+const nextSlide = () => {
+  if (currentSlide === maxSlide - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  goToSlide(currentSlide);
+  activateDot(currentSlide);
+};
+
+const prevSlide = () => {
+  if (currentSlide === 0) {
+    currentSlide = maxSlide - 1;
+  } else {
+    currentSlide--;
+  }
+  goToSlide(currentSlide);
+  activateDot(currentSlide);
+};
+
+// Initialize function
+const init = () => {
+  createDots();
+  activateDot(currentSlide);
+  goToSlide(0);
+}
+init();
+
+// Event handlers
+// Next slide  
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'ArrowLeft') prevSlide();
+  e.key === 'ArrowLeft' && prevSlide();
+  if (e.key === 'ArrowRight') nextSlide();
+}); 
+
+dotsContainer.addEventListener('click', e => {
+  if (e.target.classList.contains('dots__dot')) {
+    const { slide } = e.target.dataset;
+
+    goToSlide(slide);
+    activateDot(slide);
+  }
+});
+
 // ============================================================== Notes =============================================================
 /*
   querySelectorAll() 
