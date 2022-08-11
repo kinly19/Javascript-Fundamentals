3a24df87e5d85baa1edc104ae4c5e102d410dbaf
unknown <kinchoily@hotmail.com>
2022-08-11 21:01:16 +0100

    Add notes on 'OOP'

---
+++ 13. OOP/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// ================================================= What is Object-Oriented Programming =============================================
+/*
+  OOP
+  - Object-oriented programming is a programming paradigm based on the concept of objects.
+  - We use objects to model (describe) real-world or abstract features.
+  - Objects may contain data (properties) and code (methods). By using objects, we pack data
+    and the corresponding behavior into one block.
+  - In OOP, objects are self-contained pieces/blocks of code.
+  - Objects are building blocks of applications, and interact with one another.
+  - Interactions happens through a public interface (API): methodes that the code outside of the object
+    can access and use to communicate with the object.
+  - OOP was developed with the goal of organizing code, to make it more flexible and easier to maintain (avoid "spaghetti code").
+*/
+
+// ===================================================================================================================================
+
