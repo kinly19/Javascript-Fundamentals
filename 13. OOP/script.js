5f032cdc2e528c4162771ac4feca0ca46a564844
unknown <kinchoily@hotmail.com>
2022-08-11 21:02:13 +0100

    Add notes on 'OOP in Javascript'

--- 13. OOP/script.js
+++ 13. OOP/script.js
@@ -16,3 +16,44 @@
 
 // ===================================================================================================================================
 
+// ========================================================== OOP in Javascript ======================================================
+/*
+  OOP is done differently in Javascript.
+
+  Classical OOP: Classes
+  Class 
+  Instance
+
+  - Objects (instances) are instantiated from a class
+  - Behavior (methods) is copied from class to all instances.
+
+  OOP in Js: Prototypes
+  Prototype
+  Object
+
+  - Objects are linked to a prototype object
+  - Prototypal inheritance: The prototype contains methods (behavior) that
+    are accessible to all objects linked to that prototype.
+  - Behavior (methods) is delegated to the linked prototype object. (Prototypal inheritance/delegation)
+    
+  3 ways of implementing prototypal inheritance in javascript.
+    
+    Constructor functions
+    - Technique to create objects from functions.
+    - This is how built-in objects like arrays, maps, or sets are actually implemented.
+    ES6 Classes
+    - Modern alternative to constructor function syntax
+    - "Syntactic sugar" behind the scenes, ES6 classes work exactly like the constructor functions.
+    - ES6 classes still do NOT behave like classes in "Classical OOP". 
+    Object.create()
+    - The easiest and most straightforwardss way of linking an object to a prototype object.
+    
+    The 4 pillars of OOP are still valid
+    - Abstraction
+    - Encapsulation
+    - Inheritance
+    - Polymorphism
+    
+*/
+// ===================================================================================================================================
+
