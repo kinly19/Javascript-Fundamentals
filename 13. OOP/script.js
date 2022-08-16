1d018d2249bce114a2d9bc7c43fd642e1af4ce8c
unknown <kinchoily@hotmail.com>
2022-08-16 17:41:33 +0100

    Update notes (small challenge on OOP)

--- 13. OOP/script.js
+++ 13. OOP/script.js
@@ -245,4 +245,47 @@ console.dir(h1);
 console.dir(x => x+1);
 
 // ===================================================================================================================================  
- 
+
+// ========================================================= Coding Challenge #1 =====================================================
+/*
+  Your tasks:
+  1. Use a constructor function to implement a 'Car'. A car has a 'make' and a 
+  'speed' property. The 'speed' property is the current speed of the car in 
+  km/h
+  2. Implement an 'accelerate' method that will increase the car's speed by 10, 
+  and log the new speed to the console
+  3. Implement a 'brake' method that will decrease the car's speed by 5, and log 
+  the new speed to the console
+  4. Create 2 'Car' objects and experiment with calling 'accelerate' and 
+  'brake' multiple times on each of them
+  Test data:
+   Data car 1: 'BMW' going at 120 km/h
+   Data car 2: 'Mercedes' going at 95 km/h
+*/
+
+// Constructor
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+}
+
+// Methods
+Car.prototype.accelerate = function () {
+  // this.speed = this.speed + 10;
+  this.speed += 10;
+  if (this.speed > 300) console.log(`${this.make} ended up as a write-off`);
+  else console.log(`${this.make} going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} going at ${this.speed} km/h`);
+};
+
+// Instances
+const car1 = new Car("BMW", 120);
+const car2 = new Car("Mercedes", 95);
+
+console.log(Car.prototype.isPrototypeOf(car1)); // true
+console.log(Car.prototype.isPrototypeOf(car1) && Object.prototype.isPrototypeOf(Car) && Object.getPrototypeOf(Object.prototype) === null); // true
+// ===================================================================================================================================  
