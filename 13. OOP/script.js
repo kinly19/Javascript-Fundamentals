424a2ff5ce2e907c27e5e7943c84c3f179de91d6
unknown <kinchoily@hotmail.com>
2022-08-23 20:06:45 +0100

    Add 'Coding Challenge #3' with notes

--- 13. OOP/script.js
+++ 13. OOP/script.js
@@ -614,3 +614,73 @@ console.dir(Student.prototype.constructor);
 
 // ===================================================================================================================================
 
+// ========================================================= Coding Challenge #3 =====================================================
+/*
+  Your tasks:
+    1. Use a constructor function to implement an Electric Car (called 'EV') as a child
+    "class" of 'Car'. Besides a make and current speed, the 'EV' also has the 
+    current battery charge in % ('charge' property)
+    2. Implement a 'chargeBattery' method which takes an argument 
+    'chargeTo' and sets the battery charge to 'chargeTo'
+    3. Implement an 'accelerate' method that will increase the car's speed by 20, 
+    and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 
+    km/h, with a charge of 22%'
+    4. Create an electric car object and experiment with calling 'accelerate', 
+    'brake' and 'chargeBattery' (charge to 90%). Notice what happens when 
+    you 'accelerate'! Hint: Review the definiton of polymorphism �
+    Test data:
+    § Data car 1: 'Tesla' going at 120 km/h, with a charge of 23%
+*/
+
+const Car4 = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car4.prototype.accelerate = function () {
+  this.speed += 20;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car4.prototype.brake = function () {
+  this.speed -= 20;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const EV = function (make, speed, charge) {
+  Car4.call(this, make, speed);
+  this.charge = charge;
+};
+
+// Inherit/Link prototypes
+EV.prototype = Object.create(Car4.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+
+  console.log(`${this.make} going at ${this.speed}Km/h, with a charge of ${this.charge}%`);
+};
+
+const tesla = new EV("Tesla", 120, 23);
+tesla.chargeBattery(50);
+tesla.accelerate();
+tesla.brake();
+tesla.accelerate();
+
+console.log(tesla instanceof (EV));
+console.log(tesla instanceof (Car4));
+console.log(Car4.prototype.isPrototypeOf(EV.prototype)) // true
+
+// tesla.prototype --> EV.prototype EV.prototype --> Car4 (Because of Object.create)
+
+/*
+  Even though Car4 and EV have the exact same method names, if there are any methods or properties with the same name in a prototype chain,
+  Javascript will use the first one which appears in the prototype chain
+*/
+
+// ===================================================================================================================================
