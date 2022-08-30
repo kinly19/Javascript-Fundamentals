ed2644e3c022d47879db7f971cd9189e0a1ac012
unknown <kinchoily@hotmail.com>
2022-08-30 14:50:37 +0100

    Add logic to get and set users location (markers) with 'Leaflet' library

--- 14. Mapty/script.js
+++ 14. Mapty/script.js
@@ -10,3 +10,60 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Check if browser supports geolocation.
+if (navigator.geolocation) {
+  // Get/Set users position
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 15);
+
+      L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // Initial location marker popup
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'test',
+          })
+        )
+        .setPopupContent('You Are Here')
+        .openPopup();
+
+      // Add new marker position and popups
+      map.on('click', mapEvent => {
+        // Event coords
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+    () => {
+      alert('Could not get your position');
+    }
+  );
+}
