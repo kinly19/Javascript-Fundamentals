cf30e9a1f7d1f7964aa18075eae7b807f45792ee
unknown <kinchoily@hotmail.com>
2022-09-07 19:45:03 +0100

    Add notes on 'Callback hell'

--- 15. Asynchronous/script.js
+++ 15. Asynchronous/script.js
@@ -67,8 +67,99 @@ const getCountry = (country) => {
 // The order in which these cards show in the DOM, depends on which AJAX call finishes first
 // If we wanted these AJAX requests done in a predefined order, we could chain the request together (see 'callback hell' notes).
 
-getCountry("gb");
-getCountry("usa");
-getCountry("canada");
+// getCountry("gb");
+// getCountry("usa");
+// getCountry("canada");
+
+// ===================================================================================================================================
+
+// ======================================================= Welcome to Callback Hell ==================================================
+/*
+  Callback Hell
+  - Is essentially nested callbacks stacked below one another forming a pyramid structure. Every callback depends/waits 
+    for the previous callback, thereby making a pyramid structure that affects the readability and maintainability of the code
+*/
+
+// Example 1.
+const renderCountry = (data, className = '') => {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src=${data.flag} />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${data.population}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0]['name']}</p>
+    </div>
+  </article>  
+`;
+  // Add html into DOM
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = country => {
+  
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const neighbour = data.borders?.[0];
+
+    if (!neighbour) {
+      console.log('Do nothing');
+      return;
+    }
+
+    // Second request
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+
+      // Get neighbour country of neighbour (3 request)
+        // Get neighbour country of neighbour (4 request)
+          // Get neighbour country of neighbour (5 request)
+            // so on and so forth (Call back hell)
+    });
+  });
+}; 
+
+getCountryAndNeighbour("gb");
+// getCountryAndNeighbour("Australia");
+
+
+// Example 2.
+// Nasty stuff
+setTimeout(() => {
+  console.log('1 seconds passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+      setTimeout(() => {
+        console.log('4 seconds passed');
+        setTimeout(() => {
+          console.log('5 seconds passed');
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
 
 // ===================================================================================================================================
