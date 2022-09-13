3945b995acec23f48a545eb0db3119bb14981ffd
unknown <kinchoily@hotmail.com>
2022-09-13 19:09:18 +0100

    Add 'Asynchronous JavaScript Coding Challenge #1'

--- 15. Asynchronous/script.js
+++ 15. Asynchronous/script.js
@@ -368,3 +368,97 @@ btn.addEventListener('click', () => {
 });
 
 // ===================================================================================================================================
+
+// ======================================================== Coding Challenge #1 ======================================================
+/*
+  PART 1
+  1. Create a function 'whereAmI' which takes as inputs a latitude value ('lat') 
+  and a longitude value ('lng') (these are GPS coordinates, examples are in test 
+  data below).
+  2. Do “reverse geocoding” of the provided coordinates. Reverse geocoding means 
+  to convert coordinates to a meaningful location, like a city and country name. 
+  Use this API to do reverse geocoding: https://geocode.xyz/api. The AJAX call 
+  will be done to a URL with this format: 
+  https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and 
+  promises to get the data. Do not use the 'getJSON' function we created, that 
+  is cheating �
+  3. Once you have the data, take a look at it in the console to see all the attributes 
+  that you received about the provided location. Then, using this data, log a 
+  message like this to the console: “You are in Berlin, Germany”
+  4. Chain a .catch method to the end of the promise chain and log errors to the 
+  console
+  5. This API allows you to make only 3 requests per second. If you reload fast, you 
+  will get this error with code 403. This is an error with the request. Remember, 
+  fetch() does not reject the promise in this case. So create an error to reject 
+  the promise yourself, with a meaningful error message
+
+  PART 2
+  6. Now it's time to use the received data to render a country. So take the relevant 
+  attribute from the geocoding API result, and plug it into the countries API that 
+  we have been using.
+  7. Render the country and catch any errors, just like we have done in the last 
+  lecture (you can even copy this code, no need to type the same code)
+
+  Key
+  We want to secure and hide this key properly.
+  yourapikeyhere = 425330490038403167052x956 
+*/
+
+// Helper function fetch/error handling.
+const fetchJSON = (url, errMsg = 'Something went wrong') => {
+  return fetch(url)
+    .then(res => {
+      if (!res.ok) {
+        return res.json().then(err => {
+          console.log(err);
+          throw new Error(errMsg);
+        });
+      }
+      return res.json();
+    })
+    .then(data => {
+      if (data.error)
+        throw new Error(
+          `${errMsg}: ${data.error.description}(${data.error.code})`
+        );
+      return data;
+    });
+};
+
+const whereAmI = (lat, lng) => {
+  console.log(`Your lat:${lat}, lng:${lng}`);
+
+  // Example 1. With helper function.
+  fetchJSON(`https://geocode.xyz/${lat}, ${lng}?geoit=json&auth=yourapikeyhere`)
+    .then(data => fetchJSON(`https://restcountries.com/v2/name/${data.country}`,'Country not found'))
+    .then(data2 => renderCountry(data2[0]))
+    .catch(err => countriesContainer.insertAdjacentText('beforeend', err.message))
+    .finally(() => (countriesContainer.style.opacity = 1));
+
+
+  /* Example 2. direct without helper function
+  fetch(`https://geocode.xyz/${lat}, ${lng}?geoit=json&auth=youapikeyhere`)
+    .then(res => {
+      if (!res.ok) throw new Error(res.statusText);
+      return res.json();
+    })
+    .then(data => {
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(res.statusText);
+      return res.json();
+    })
+    .then(data2 => renderCountry(data2[0]))
+    .catch(err =>
+      countriesContainer.insertAdjacentText('beforeend', err.message)
+    )
+    .finally(() => (countriesContainer.style.opacity = 1));
+  */
+};
+
+whereAmI(52.508, 13.381); //bi
+whereAmI(19.037, 72.873); // gm
+whereAmI(37.090240, -95.712891); // usa
+
+// ===================================================================================================================================
