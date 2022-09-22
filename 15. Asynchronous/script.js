593f7468d48f38ed6e211fd16243fe3d35e14f92
unknown <kinchoily@hotmail.com>
2022-09-22 19:43:19 +0100

    Add Coding Challenge #3

--- 15. Asynchronous/script.js
+++ 15. Asynchronous/script.js
@@ -1108,3 +1108,100 @@ Promise.any([
   .then(data => console.log(data));
 
 // ===================================================================================================================================
+
+// ========================================================= Coding Challenge #3 =====================================================
+/*
+  Your tasks:
+  PART 1
+  1. Write an async function 'loadNPause' that recreates Challenge #2, this time 
+  using async/await (only the part where the promise is consumed, reuse the 
+  'createImage' function from before)
+  2. Compare the two versions, think about the big differences, and see which one 
+  you like more
+  3. Don't forget to test the error handler, and to set the network speed to “Fast 3G”
+  in the dev tools Network tab
+
+  PART 2
+  1. Create an async function 'loadAll' that receives an array of image paths 
+  'imgArr'
+  2. Use .map to loop over the array, to load all the images with the 
+  'createImage' function (call the resulting array 'imgs')
+  3. Check out the 'imgs' array in the console! Is it like you expected?
+  4. Use a promise combinator function to actually get the images from the array �
+  5. Add the 'parallel' class to all the images (it has some CSS styles)
+  Test data Part 2: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function
+*/
+
+const imgContainer = document.querySelector(".images");
+let currentImage;
+
+// Handlers
+const wait = seconds => {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const createImage = imgPath => {
+  // 1. Return promise
+  return new Promise((resolve, reject) => {
+    // 2. Create img element
+    const img = document.createElement('img');
+    img.src = imgPath;
+    // 3. Append image to DOM when image has loaded
+    img.addEventListener('load', () => {
+      imgContainer.append(img);
+      currentImage = img;
+      // 4. set resolve value 
+      resolve(img);
+    });
+    // 5. Handle error
+    img.addEventListener('error', () => {
+      reject(new Error('image not found'))
+    });
+  });
+};
+
+const loadAll = async imgArr => {
+  try {
+    // Create an array and load images with createImage
+    const imgs = imgArr.map(async img => await createImage(img));
+    console.log(imgs);
+
+    // Create an array of all promise results
+    const imgEl = await Promise.all(imgs);
+    // use promise.all to get the actual values of each promise
+    console.log(imgEl);
+
+    //Add a class style to each image element
+    imgEl.forEach(imgEl => {
+      imgEl.classList.add('parallel');
+    });
+  } catch (err) {
+    // (side note on errors the remaining images are still appended into the DOM with createImage which is why no styles are added to images)
+    throw new Error(err);
+  }
+};
+
+const loadNPause = async () => {
+  try {
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 has loaded');
+    currentImage = img;
+    await wait(2);
+    currentImage.style.display = 'none';
+
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 2 has loaded');
+    currentImage = img;
+    await wait(2);
+    currentImage.style.display = 'none';
+    
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+// loadNPause();
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
+// ===================================================================================================================================
