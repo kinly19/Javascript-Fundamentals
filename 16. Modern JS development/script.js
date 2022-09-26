41e93390625c46d1559b3c0103f7372a2bbed70b
unknown <kinchoily@hotmail.com>
2022-09-26 20:52:45 +0100

    Add notes on 'Exporting and Importing in ES6 Modules'

--- 16. Modern JS development/script.js
+++ 16. Modern JS development/script.js
@@ -33,3 +33,44 @@
   - File downloading: Synchronous.
 */
 // ===================================================================================================================================
+
+// ================================================ Exporting and Importing in ES6 Modules ===========================================
+
+// To connect an imported module, we need to specify the script type within the HTML file (<script type="module" defer src="script.js"></script>)
+
+// Importing module.
+import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// (totalPrice as price) Changing the name of a named import.
+
+console.log("Importing module");
+/* console log returns
+Exporting module
+Importing module
+
+All the code within script.js is parsed and before it is executed, any module
+which it imports is executed first. (All imported modules (shoppingCart.js) are executed first).
+If our imported module (shoppingCart.js) has a top level await it will code block script.js until it has finished the blocking code.
+*/
+
+// Variables within modules are scoped to the module unless we import them from the module (must be exported from the module itself too).
+// console.log(shippingCode) // shippingCode is not defined
+addToCart("Apple", 150);
+console.log(price, tq);
+
+// Import everything from a module (everything which has been exported from specified module).
+import * as shoppingCart from './shoppingCart.js';
+shoppingCart.addToCart("Orange", 250);
+
+// Using Default exports
+import add from './shoppingCart.js';
+add("Lemon", 50);
+
+/* 
+We can also mix Default exports with Named exports but not advised.
+
+import add, {totalPrice} from './shoppingCart.js';
+add("Lemon", 50);
+console.log(totalPrice);
+*/
+
+// ===================================================================================================================================
